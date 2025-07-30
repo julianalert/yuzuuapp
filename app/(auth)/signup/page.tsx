3cc8f9e73c4c2332516d56a3d78f64124a7f38d4
JulienD <juliend@JulienDs-MacBook-Pro.local>
2025-07-30 16:03:50 +0100

    auth system update + workflows

--- app/(auth)/signup/page.tsx
+++ app/(auth)/signup/page.tsx
@@ -2,11 +2,6 @@
 
 import SignUpForm from '../../../components/auth/signup-form'
 
-export const metadata = {
-  title: "Sign Up - Simple",
-  description: "Page description",
-};
-
 export default function SignUp() {
   return <SignUpForm />
 }
